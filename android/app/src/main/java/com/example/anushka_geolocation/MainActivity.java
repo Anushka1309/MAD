ab00e4012d9bbe46ea8290602984fdb3435d78c0
Anushka1309 <iit2020077@iiita.ac.in>
2021-03-14 07:46:43 +0530

    'draft'

---
+++ android/app/src/main/java/com/example/anushka_geolocation/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.anushka_geolocation;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/app.dart
@@ -0,0 +1,83 @@
+import 'package:anushka_geolocation/authentication/bloc/authentication_bloc.dart';
+import 'package:anushka_geolocation/home/view/home_page.dart';
+import 'package:anushka_geolocation/login/view/login_page.dart';
+import 'package:anushka_geolocation/splash/view/splash_page.dart';
+import 'package:anushka_geolocation/theme.dart';
+import 'package:authentication_repository/authentication_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'adminpage.dart';
+
+class App extends StatelessWidget {
+  const App({
+    Key key,
+    @required this.authenticationRepository,
+  })  : assert(authenticationRepository != null),
+        super(key: key);
+
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return RepositoryProvider.value(
+      value: authenticationRepository,
+      child: BlocProvider(
+        create: (_) => AuthenticationBloc(
+          authenticationRepository: authenticationRepository,
+        ),
+        child: AppView(),
+      ),
+    );
+  }
+}
+
+class AppView extends StatefulWidget {
+  @override
+  _AppViewState createState() => _AppViewState();
+}
+
+class _AppViewState extends State<AppView> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  NavigatorState get _navigator => _navigatorKey.currentState;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: theme,
+      navigatorKey: _navigatorKey,
+      builder: (context, child) {
+        return BlocListener<AuthenticationBloc, AuthenticationState>(
+          listener: (context, state) {
+            switch (state.status) {
+              case AuthenticationStatus.authenticated:
+                if (state.user.email == 'admin@admin.com') {
+                  _navigator.pushAndRemoveUntil<void>(
+                    AdminPage.route(),
+                    (route) => false,
+                  );
+                } else
+                  _navigator.pushAndRemoveUntil<void>(
+                    HomePage.route(),
+                    (route) => false,
+                  );
+                break;
+              case AuthenticationStatus.unauthenticated:
+                _navigator.pushAndRemoveUntil<void>(
+                  LoginPage.route(),
+                  (route) => false,
+                );
+                break;
+              default:
+                break;
+            }
+          },
+          child: child,
+        );
+      },
+      onGenerateRoute: (_) => SplashPage.route(),
+    );
+  }
+}
